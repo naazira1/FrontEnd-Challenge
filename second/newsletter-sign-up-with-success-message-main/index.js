8836bcf79a5b8a5277016f9ec03e9513169efd2e
naazira1 <135894752+naazira1@users.noreply.github.com>
2023-10-09 08:46:54 +0000

    buliding my js skills
    news letter  website that allows users to subscribe to daily news

---
+++ second/newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,13 @@
+
+ let dismiss=document.querySelector('.dismis-message');
+ dismiss.addEventListener('click',function(){
+     window.location.href = "file:///C:/Users/NAAZIRA/Desktop/frontEnd%20challenfges/second/newsletter-sign-up-with-success-message-main/index.html?#"
+
+ })
+ const email=document.querySelector('.form-label');
+ let form=document.querySelector('.form');
+ form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+})
+
